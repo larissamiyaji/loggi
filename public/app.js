2f72fd2fe69e8d709fe53100e485f268277b25dc
Camila <camila@heidy-pc.local>
2020-09-10 10:51:52 -0300

    dark mode

--- public/app.js
+++ public/app.js
@@ -4,7 +4,86 @@ async function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 13,
     center: { lat: -23.5489, lng: -46.6388 },
-    mapTypeId: "satellite"
+    styles: [
+      { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+      {
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }]
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }]
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [{ color: "#263c3f" }]
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#6b9a76" }]
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{ color: "#38414e" }]
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#212a37" }]
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#9ca5b3" }]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ color: "#746855" }]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#1f2835" }]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#f3d19c" }]
+      },
+      {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [{ color: "#2f3948" }]
+      },
+      {
+        featureType: "transit.station",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }]
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [{ color: "#17263c" }]
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#515c6d" }]
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [{ color: "#17263c" }]
+      }
+    ]
   });
   const response = await consumirApi(endpoint, Appquery)
   const getPoints = response.data.closestDrivers.drivers.map((item) => {
@@ -35,7 +114,7 @@ readyDrivers.innerText = countReadyDrivers
   var central = new google.maps.LatLng( -23.5489, -46.6388)
   var west = new google.maps.LatLng(-23.5719, -46.7008)
   
-/*   var coordenadas = [south, east, north, central, west];
+  var coordenadas = [south, east, north, central, west];
 var flightPath=new google.maps.Polygon({
   path:coordenadas,
   strokeColor:"#0000FF",
@@ -45,7 +124,7 @@ var flightPath=new google.maps.Polygon({
   fillOpacity:0.4
   });
   
-  flightPath.setMap(map); */
+  flightPath.setMap(map);
 
 
 }
